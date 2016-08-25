59a06a0390de2e4447149ea878951051eb8e5a59
Jason Pelletier <jpelletier19@me.com>
2016-08-25 11:26:26 -0500

    migration to remove type_id from user table

--- dbc_mentor/db/schema.rb
+++ dbc_mentor/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825013959) do
+ActiveRecord::Schema.define(version: 20160825162125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20160825013959) do
     t.string   "password_digest"
     t.integer  "phone"
     t.string   "type"
-    t.integer  "type_id"
     t.string   "topics"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
