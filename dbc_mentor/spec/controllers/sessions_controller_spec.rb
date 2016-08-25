0a56a16accc85ff3aa67d7c528266ad2707816d6
Tolu Oyeniyi <tjoye20@gmail.com>
2016-08-25 16:35:24 -0500

    Finish tests for sessions controller.

--- dbc_mentor/spec/controllers/sessions_controller_spec.rb
+++ dbc_mentor/spec/controllers/sessions_controller_spec.rb
@@ -8,7 +8,7 @@ describe SessionsController do
         { :email => user.email, :password => user.password }
       end
 
-    it "logs in the coorect user and sets a session" do
+    it "logs in the correct user and sets a session" do
       post :create, params
       expect(response).to have_http_status 302
       expect(session[:user_id]).to eq(user.id)
@@ -24,13 +24,15 @@ describe SessionsController do
       post :create, params
       expect(session[:user_id]).to_not eq(user.id)
       expect(flash[:alert]).to eq("Invalid email or password")
+      expect(response).to render_template("sessions/new", "layouts/application")
     end
   end
 
-  context "when you click the logout button" do
+
     it "logs you out and clears out the session" do
-
+      post :destroy
+      expect(session[:user_id]).to eq nil
+      expect(flash[:notice]).to eq("Logged out!")
+      expect(response).to redirect_to root_url
     end
-  end
-
 end
