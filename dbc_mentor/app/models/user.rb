bac4677c592330d123d50193b2294bd348292512
Jessica Bohn <bohnjessica15@gmail.com>
2016-08-24 21:47:53 -0500

    Add new gem to Gemfile for coverage
    Rails does not automatically calculate the coverage of your tests
    throughout your app, which is why it was necessary to install the
    gem 'simplecov' which uses Ruby's built-in Coverage to give the
    developer a code coverage analysis to see where there are gaps in
    their testing.
    
    - Add 'simplecov' gem to Gemfile
    - Add SimpleCov.start to the top of the spec_helper

--- dbc_mentor/app/models/user.rb
+++ dbc_mentor/app/models/user.rb
@@ -1,5 +1,4 @@
-class User < ActiveRecord::Base 
-
+class User < ActiveRecord::Base
 	has_secure_password
-	validates :username, :password_digest, :email, :type, :type_id, presence: true
+	validates :username, :password_digest, :email, :type, presence: true
 end
