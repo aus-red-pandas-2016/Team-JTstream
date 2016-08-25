bdb7d17b446700f5d49ac7546b0599549b36971c
Jason Pelletier <jpelletier19@me.com>
2016-08-25 16:35:07 -0500

    user can register as either student or mentor.  form checks to ensure that passwords match.  hardcoded mentor code in and checks this as well before making a mnetor user

--- dbc_mentor/app/controllers/users_controller.rb
+++ dbc_mentor/app/controllers/users_controller.rb
@@ -2,7 +2,35 @@ class UsersController < ApplicationController
 
 	def new
 		@user = User.new
-		
 	end
+
+	def index
+		@user = User.find(session[:id])
+	end
+
+	def create
+		
+		password = params[:user][:password]
+		password_confirm = params[:user][:password_confirmation]
+		if password != password_confirm
+			flash.now[:alert] = "Passwords Do Not Match"
+			@user = User.new
+			return render "new" 
+		else
+			if params[:type] == "Mentor"
+				if params[:mentor_code] != "12345"
+					@user = User.new
+					flash.now[:alert] = "Mentor Code Is Not Valid.  Try Again"
+					return render "new"
+				end
+			end
+			@user = User.create!(name: params[:user][:name], email: params[:user][:email], password: password, phone: params[:user][:phone], type: params[:type], topics: params[:user][:topics])
+			session[:id] = @user.id
+			flash[:notice] = "Registration Successful!  Thank You For Joining"
+			redirect_to users_path
+		end
+	end
+
+
 
 end
--- dbc_mentor/app/models/user.rb
+++ dbc_mentor/app/models/user.rb
@@ -3,8 +3,4 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :name, :password_digest, :email, :type, presence: true
 
-	def mentor_code
-		return ""
-	end
-
 end
