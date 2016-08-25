d4e6c6f05499aaa0d2dd47262f041623fc254c29
Jessica Bohn <bohnjessica15@gmail.com>
2016-08-25 14:51:42 -0500

    Update users table in schema
    The phone attribute for a User had the wrong input type of integer,
    which needed to be changed to string to save correctly. Two
    migrations were generated using rails to solve this problem. The
    first migration removed the existing phone column from the users
    table/class while the second re-added the phone column, but with
    an input type of string. Once created, but files were migrated
    and the schema successfully updated and will now work correctly.
    
    - Generate and run migration for removing phone column
    - Generate and run new migration for adding new phone column

--- dbc_mentor/db/schema.rb
+++ dbc_mentor/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825013959) do
+ActiveRecord::Schema.define(version: 20160825194907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,12 +19,12 @@ ActiveRecord::Schema.define(version: 20160825013959) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.integer  "phone"
     t.string   "type"
     t.integer  "type_id"
     t.string   "topics"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "phone"
   end
 
 end
