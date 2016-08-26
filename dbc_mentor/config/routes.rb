4df7427924a67e578c54e0d8512c2d22ded9ad0b
Jessica Bohn <bohnjessica15@gmail.com>
2016-08-26 11:07:03 -0500

    Add routes for feedback
    https://trello.com/c/1huOCFwi
    
    Since the feedback model and controller were added and the users
    need access to the the feedback links to create and submit their
    own feedback, resource routes for feedbacks were created in the
    routes file. However, users currently only need 'routes' for new
    and create actions of the controller, thus they were added using
    and only statement for the resources :feedbacks.
    
    - Create '/feedbacks/new' resource route/path
    - Create '/feedbacks/create' resource route/path

--- dbc_mentor/config/routes.rb
+++ dbc_mentor/config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
+
   root :to => "sessions#new"
   resources :users
   resources :sessions
+
 
 end
