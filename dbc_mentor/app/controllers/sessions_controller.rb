3fa5e8ee2b60a09984f55d6f760ae02c1c9f7871
Tolu Oyeniyi <tjoye20@gmail.com>
2016-08-26 15:18:33 -0500

    Fix code to pass tests.

--- dbc_mentor/app/controllers/sessions_controller.rb
+++ dbc_mentor/app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    if user
-      if user.authenticate(params[:password])
+    # if user
+      if user && user.authenticate(params[:password])
         session[:id] = user.id
         redirect_to root_url, :notice => "Logged in!"
-      end 
+      # end
     else
       flash.now.alert = "Invalid email or password"
       render "new"
